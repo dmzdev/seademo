a5e4212805a3eeab50f8aa92112138046df7eda1
Scott Shillcock <scott@shillcock.net>
2010-12-29 11:22:19 -0800

    first commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. ../scripts/envsetup.sh
+
+export DMZ_APP_NAME=saedemo
+
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/canvas.xml config/input.xml config/js.xml config/version.xml $*
